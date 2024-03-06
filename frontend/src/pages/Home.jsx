394aed8bead168944c0a80db2be239ec966530f6
Tharu60 <115988213+Tharu60@users.noreply.github.com>
2024-03-07 02:30:48 +0530

    Service card adding

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,12 @@ import heroImg01 from '../assets/images/hero-img01.png';
 import heroImg02 from '../assets/images/hero-img02.png';
 import heroImg03 from '../assets/images/hero-img03.png';
 import icon01 from '../assets/images/icon01.png';
+import icon02 from '../assets/images/icon02.png';
+import icon03 from '../assets/images/icon03.png';
 import { Link } from 'react-router-dom'; 
 import {BsArrowRight} from 'react-icons/bs';
+import About from "../components/About/About";
+import ServiceList from "../components/Services/ServiceList";
 
 
 
@@ -97,9 +101,61 @@ const Home = () => {
                   </Link>
               </div>
               </div>            
+              <div className="py-[30px] px-5">
+              <div className="flex item-center justify-center"><img src={icon02} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Find A Location
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-class care for everyone. Our health System offers unmatched, expert health care.
+                  </p>
+
+                  <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                    <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                  </Link>
+              </div>
+              </div>
+              <div className="py-[30px] px-5">
+              <div className="flex item-center justify-center"><img src={icon03} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Book Appointment
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-class care for everyone. Our health System offers unmatched, expert health care.
+                  </p>
+
+                  <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                    <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                  </Link>
+              </div>
+              </div>
           </div>
         </div>
       </section>
+
+      <About />
+
+      {/* ========= Services Section ======= */}
+
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Medical Services</h2>
+            <p className="text_para text-center">
+              World-class care for everyone. Our health System offers unmatched, expert health care.
+            </p>
+          </div>
+
+          <ServiceList />
+        </div>
+      </section>
+
+      {/* ========= Services Section end ======= */}
+
     </>
   );
 };
