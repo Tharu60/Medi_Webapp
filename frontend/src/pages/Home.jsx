4716596890abeab75db0c18a9e92e96a243eb9bb
Tharu60 <115988213+Tharu60@users.noreply.github.com>
2024-03-09 00:34:57 +0530

    FAQ section adding

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,11 +9,13 @@ import icon03 from '../assets/images/icon03.png';
 import featureImg from '../assets/images/feature-img.png'
 import videoIcon from '../assets/images/video-icon.png'
 import avatarIcon from '../assets/images/avatar-icon.png'
-import { Link } from 'react-router-dom'; 
-import {BsArrowRight} from 'react-icons/bs';
+import faqImg from '../assets/images/faq-img.png'
+import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/Faq/FaqList";
 
 
 
@@ -98,14 +100,14 @@ const Home = () => {
                 </h2>
                 <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
                   World-class care for everyone. Our health System offers unmatched, expert health care.
-                  </p>
+                </p>
 
-                  <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
-                    <BsArrowRight className="group-hover:text-white w-6 h-5" />
-                  </Link>
+                <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
               </div>
-              </div>            
-              <div className="py-[30px] px-5">
+            </div>
+            <div className="py-[30px] px-5">
               <div className="flex item-center justify-center"><img src={icon02} alt="" />
               </div>
               <div className="mt-[30px]">
@@ -114,14 +116,14 @@ const Home = () => {
                 </h2>
                 <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
                   World-class care for everyone. Our health System offers unmatched, expert health care.
-                  </p>
+                </p>
 
-                  <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
-                    <BsArrowRight className="group-hover:text-white w-6 h-5" />
-                  </Link>
+                <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
               </div>
-              </div>
-              <div className="py-[30px] px-5">
+            </div>
+            <div className="py-[30px] px-5">
               <div className="flex item-center justify-center"><img src={icon03} alt="" />
               </div>
               <div className="mt-[30px]">
@@ -130,13 +132,13 @@ const Home = () => {
                 </h2>
                 <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
                   World-class care for everyone. Our health System offers unmatched, expert health care.
-                  </p>
+                </p>
 
-                  <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
-                    <BsArrowRight className="group-hover:text-white w-6 h-5" />
-                  </Link>
+                <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
               </div>
-              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -186,46 +188,47 @@ const Home = () => {
               </Link>
             </div>
 
-          {/* ========= Feature img ======= */}
-          <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
-            <img src={featureImg} className="w-3/4" alt="" />
+            {/* ========= Feature img ======= */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} className="w-3/4" alt="" />
 
-            <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:botton-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:botton-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
 
-              <div className="flex item-center justify-between">
-                <div className="flex item-center gap-[6px] lg:gap-3">
-                  <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
-                    Tue, 24
-                  </p>
-                  <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
-                    10:00 AM
-                  </p>
+                <div className="flex item-center justify-between">
+                  <div className="flex item-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10:00 AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                    <img src={videoIcon} alt="" />
+                  </span>
                 </div>
-                <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
-                  <img src={videoIcon} alt="" />
-                </span>
-              </div>
-              <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
-                Consultation
-              </div>
+                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                  Consultation
+                </div>
 
-              <div className="flex item-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
-                <img src={avatarIcon} alt="" />
-                <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
-                  Wayne Collins
-                </h4>
+                <div className="flex item-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="" />
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                    Wayne Collins
+                  </h4>
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
       </section>
 
       {/* ========= Feature Section end ======= */}
+
       {/* ========= Our grate doctors ======= */}
       <section>
         <div className="container">
-        <div className="xl:w-[470px] mx-auto">
+          <div className="xl:w-[470px] mx-auto">
             <h2 className="heading text-center">Our Grat Doctors</h2>
             <p className="text_para text-center">
               World-class care for everyone. Our health System offers unmatched, expert health care.
@@ -235,6 +238,25 @@ const Home = () => {
         </div>
       </section>
       {/* ========= Our grate doctors end ======= */}
+      {/* ========= FAQ section ======= */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="" />
+            </div>
+            
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ========= FAQ section end ======= */}
 
 
     </>
